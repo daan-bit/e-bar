7690b4b929c5dcda8869158c1e51f0364ceff22f
s119605 <s119605@student.hsleiden.nl>
2021-12-07 20:03:13 +0100

    error message + eerste 4 pagina's interactief

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
+let doorgaan = false;
+
+//Selecteren van drank of frisdrank
 function changeBg(item){
 
-    //Selecteren van een listitem
     let list = document.querySelectorAll(".list__listItem");
 
     for(let i = 0; i < list.length; i++){
@@ -10,8 +12,11 @@ function changeBg(item){
     
     let child = item.children[0];
     child.style.backgroundColor = "#eee";
+    doorgaan = true;
 
-    if(item.childNodes[3].textContent.trim() === "Fanta" || item.childNodes[3].textContent.trim() === "Cola"){
+
+    //Opslaan in van soort drank of frisdrank in local storage
+    if(item.childNodes[3].textContent.trim() === "Sinas" || item.childNodes[3].textContent.trim() === "Cola"){
         setFris(item);
     }else{
         setDrank(item);
@@ -19,33 +24,57 @@ function changeBg(item){
     
 }
 
+//Checkt of er een keuze is gemaakt
+function check(pagina){
+    
+    if(doorgaan === true){
+        if(pagina == 'drank'){
+            window.location = "kiesFris.html";
+        }else{
+            window.location = "laden.html";
+        }
+        
+    }else{
+        const error = document.getElementById("error");
+        error.style.display = "flex";
+        setTimeout(closeError, 1000);
+    }
+
+}
+
+function closeError(){
+    const error = document.getElementById("error");
+    error.style.display = "none";
+}
+
+//Gekozen drank opslaan in local storage
 function setDrank(item){
-    //Ophalen soort drank en zetten in local storage
     let drank = item.childNodes[3].textContent.trim();
     localStorage.setItem("drank", drank);
 }
 
+//Gekozen frisdrank opslaan in local storage
 function setFris(item){
-    //Ophalen soort frisdrank en zetten in local storage
     let fris = item.childNodes[3].textContent.trim();
     localStorage.setItem("fris", fris);
 }
 
 //Veranderd tekst en kleur in box naar gekozen frisdrank
 function changeText(){
-    let box = document.getElementById("box")
-    box.innerText = localStorage.getItem("fris"); 
+    let charFris = localStorage.getItem("fris").substring(0,2);
+    let charDrank = localStorage.getItem("drank").substring(0,2);
+
+    let box = document.getElementById("box");
+
+    box.innerText = charDrank + charFris; 
 
     let soort = localStorage.getItem("fris");
     
-
-    if(soort === "Fanta"){
+    //Veranderd kleur in fill naar soort frisdrank
+    if(soort === "Sinas"){
         box.style.setProperty('--fill', '#EB6A0A');
     }else if(soort === "Cola"){
         box.style.setProperty('--fill', '#202020');
     }   
     
-    console.log("fris " + localStorage.getItem("fris"));
-    console.log("drank " + localStorage.getItem("drank"));
-
 }
