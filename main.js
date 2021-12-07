f8e4b0e519eb928293b8251e6fbf3b5f6be44d64
s119605 <s119605@student.hsleiden.nl>
2021-12-07 14:30:12 +0100

    interactieve fill mode

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 function changeBg(item){
+
+    //Selecteren van een listitem
     let list = document.querySelectorAll(".list__listItem");
 
     for(let i = 0; i < list.length; i++){
@@ -8,4 +10,42 @@ function changeBg(item){
     
     let child = item.children[0];
     child.style.backgroundColor = "#eee";
+
+    if(item.childNodes[3].textContent.trim() === "Fanta" || item.childNodes[3].textContent.trim() === "Cola"){
+        setFris(item);
+    }else{
+        setDrank(item);
+    }
+    
+}
+
+function setDrank(item){
+    //Ophalen soort drank en zetten in local storage
+    let drank = item.childNodes[3].textContent.trim();
+    localStorage.setItem("drank", drank);
+}
+
+function setFris(item){
+    //Ophalen soort frisdrank en zetten in local storage
+    let fris = item.childNodes[3].textContent.trim();
+    localStorage.setItem("fris", fris);
+}
+
+//Veranderd tekst en kleur in box naar gekozen frisdrank
+function changeText(){
+    let box = document.getElementById("box")
+    box.innerText = localStorage.getItem("fris"); 
+
+    let soort = localStorage.getItem("fris");
+    
+
+    if(soort === "Fanta"){
+        box.style.setProperty('--fill', '#EB6A0A');
+    }else if(soort === "Cola"){
+        box.style.setProperty('--fill', '#202020');
+    }   
+    
+    console.log("fris " + localStorage.getItem("fris"));
+    console.log("drank " + localStorage.getItem("drank"));
+
 }
